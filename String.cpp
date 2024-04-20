729b59901abe6038959391c52a4253e1a26e0b8e
jvrsarma <36181990+jvrsarma@users.noreply.github.com>
2024-04-20 19:42:35 -0400

    Improved String.cpp (Update1)
    Improved version of String class implementation - version 2.0

--- String.cpp
+++ String.cpp
@@ -3,130 +3,155 @@
 #include <cstring>
 #include <cstdlib>
 using namespace std;
+
 // New String class.
-class String {
-    char* cPtr;         // Char pointer to store data.
-public:
-    String() : cPtr(nullptr) {};   // Default constructor.
-    String(const char* ch) : cPtr(new char[strlen(ch) + 1]) { strcpy(cPtr, ch); }    // Patameter constructor to store data.
-    String(String& st)      // Copy constructor to copy the data from one object to this object.
-    {
-        this->cPtr = new char[strlen(st.cPtr) + 1];
-        strcpy(this->cPtr, st.cPtr);
-    }
-    friend ostream& operator<<(ostream& os, const String& st)   // Operator to display data.
-    {
-        os << st.cPtr << ends;
-        return os;
-    }
-    ~String() { if (cPtr != nullptr) delete[] cPtr; cPtr = nullptr; }   // Destructor to clear data.
-    String& operator=(String& st)   // Assignment operator to assign this object with existing other object.
-    {
-        if (this->cPtr == nullptr) { delete[] this->cPtr; this->cPtr = nullptr; }
-        this->cPtr = new char[strlen(st.cPtr) + 1];
-        strcpy(this->cPtr, st.cPtr);
-        return *this;
-    }
-
-    String& operator+=(const String& st)    // Append new string to
-    {
-        char* cTemp = nullptr;
-        if (this->cPtr != nullptr)
+class String
+{
+        char* cPtr;             // Char pointer to store data.
+        int capacity;           // Capacity required to store data character by character.
+        void reallocate()       // Resize the cPtr (character storgate) variable to store data character by character.
         {
-            cTemp = new char[strlen(this->cPtr) + strlen(st.cPtr) + 1];
-            strcpy(cTemp, this->cPtr);
-            strcat(cTemp, st.cPtr);
-            delete[] this->cPtr; this->cPtr = nullptr;
+            char *cTemp = nullptr;      // Temporary storage location before resize cPtr.
+            if (this->cPtr != nullptr)  // If cPtr has data 
+            {
+                cTemp = new char[strlen(this->cPtr)+1];     // Allocate the same size of memeory as cPtr 
+                strcpy(cTemp, this->cPtr);                  // Copy cPtr data into temporary cTemp location.
+                delete[] this->cPtr; this->cPtr = nullptr;  // Delete cPtr.
+            }
+            this->cPtr = new char[capacity];    // Now resize cPtr to new extnded size.
+            strcpy(this->cPtr, cTemp );         // Copy the old data back to cPtr.
         }
-        else
-        {
-            cTemp = new char[strlen(st.cPtr) + 1];
-            strcpy(cTemp, st.cPtr);
-        }
-        this->cPtr = new char[strlen(cTemp) + 1];
-        strcpy(this->cPtr, cTemp);
-        delete[] cTemp, cTemp = nullptr;
-        return *this;
-    }
-
-    class iterator {
-        char* ptr;
     public:
-        iterator(char* pd) : ptr(pd) {};        // Store add ress of the element;
-        // Overload operators == or != for iterator movements
-        friend bool operator==(const iterator& it1, const iterator& it2) { return strcmp(it1.ptr, it2.ptr); }
-        friend bool operator!=(const iterator& it1, const iterator& it2) { return strcmp(it1.ptr, it2.ptr); }
-
-        // Overload prefix operator ++ to move forward.
-        iterator& operator++()
+        String() : cPtr(nullptr), capacity(0) {}        // Default constructor to create an empty object and initialize cPtr and capacity.
+        String(const char* cDat) : cPtr(new char[strlen(cDat) + 1]), capacity(0)  { strcpy(this->cPtr, cDat); }  // Construct object during declaration with data.
+        ~String() { if (cPtr != nullptr) delete[] cPtr; cPtr = nullptr; } // Destructor to deallocate memory allocated to cPtr
+        friend ostream& operator<<(ostream& os, const String& str)  // Overloading operator << to print object.
         {
-            ++ptr;
-            return *this;
+            os << str.cPtr<< ends;      // Redirect cPtr into ostream object and 
+            return os;                  // return the ostream object.
         }
-        // Overload postfix operator ++ to move forward
-        iterator& operator++(int)
+        String(String& str)         // Copy constructor to initialize this object with the existing object.
         {
-            iterator* temp = this;
-            ++(*this);
-            return *temp;
+            this->cPtr = new char[strlen(str.cPtr) + 1];
+            strcpy(this->cPtr, str.cPtr);
         }
-        // Overload operator * to provide actual value at the address
-        char& operator*() { return *this->ptr; }
-        friend iterator operator+(const iterator& it, int iPos) { return iterator(it.ptr + iPos); }
-        friend iterator operator-(const iterator& it, int iPos) { return iterator(it.ptr - iPos); }
-    };
-
-    // Get the begining of the character data memory loation.
-    iterator begin() { return iterator(&this->cPtr[0]); }
-    // Get the end of the character data memory loation.
-    iterator end() { return iterator(&this->cPtr[strlen(cPtr)]); }
+        String& operator=(String& str)  // Assignment operator to transfer data from another object into this object.
+        {
+            if (this->cPtr != nullptr) delete[] this->cPtr; // First clear the existing data (if any) by deallocating the memory
+            this->cPtr = new char[strlen(str.cPtr)+1];      // Allocate the memory for cPtr with the same size as other object's cPtr.
+            strcpy(this->cPtr, str.cPtr);                   // Copy the other object's cPtr data into this cPtr.
+            return *this;                                   // Return the current object.
+        }
+        String& operator+=(const char* cDat)                // Overloading += operator to append this object with supplied characters data. 
+        {
+            char* cTemp = nullptr;      // Declare a Temp pointer variable
+            if (this->cPtr != nullptr)  // If cPtr has data
+            {
+                cTemp = new char[strlen(this->cPtr) +  strlen(cDat) + 1];   // Then allocate equivalent memory size to a temporary variable with the total size of cPtr+cDat
+                strcpy(cTemp, this->cPtr);          // Copy the cPtr's data into temporary memory cTemp
+                strcat(cTemp, cDat);                // Concatenate the cDat (new data) to the temporary (cTemp) variable.
+                delete[] this->cPtr; this->cPtr = nullptr;  // deallocate cPtr's memory with delete operator.
+            } else          // If this cPtr is empty
+            {
+                cTemp = new char[strlen(cDat) + 1];     // Allocate equal memory size to a temporary variable with the size of cDat
+                strcpy(cTemp, cDat);                    // Copy the cDat (new data) to the temporary (cTemp) variable.
+            }
+            this->cPtr = new char[strlen(cTemp) + 1];   // Now reallocate memory for this cPtr same as temporary (cTemp) variable.
+            strcpy(this->cPtr, cTemp);                  // Copy the data from temporary (cTemp) memory into this cPtr.
+            delete[] cTemp; cTemp = nullptr;            // Deallocate temporary memory
+            return *this;                               // Return this.
+        }
+        
+        String& operator+=(const char ch)               // Overloade operator += to sore character by character.
+        {
+            if (this->capacity < strlen(this->cPtr) + 1)    // If the capacity of cPtr is less than data stored (length of cPtr)
+            {
+                this->capacity = strlen(this->cPtr) + 10;   // increase the capacity by 10.
+                reallocate();                               // and resize the cPtr's memory.
+            }
+            
+            if (this->cPtr != nullptr)          // If the reallocation / resize is succssful
+            {
+                this->cPtr[strlen(this->cPtr)]=ch;  // Then store the character.
+            }
+            return *this;                       // Return the updated this object.
+        }
+        
+        // Iterator
+        class Iterator 
+        {
+                char* ptr;      // Local pointer object to hold the address of the memory location.
+            public:
+                Iterator(char* p) : ptr(p) {}   // Store address of the element
+                Iterator operator++()           // Overload prefix operator ++ to move forward.
+                {
+                    ++ptr;                      // Advance the address by one whenever operator ++ is called.
+                    return *this;               // Return this iterator object after increment.
+                }
+                // Overload postfix operator ++ to move forward
+                Iterator& operator++(int)
+                {
+                    Iterator* temp = this;  // Copy this iterator to temporary iterator to hold status before the change.
+                    ++(*this);              // Increment this iterator
+                    return *temp;           // Return the old iterator data.
+                }
+                char& operator*() { return *this->ptr; }    // Overload operator * to retrieve the value of the current memory pointed by this iterator object.
+                // Overload operators == or != for iterator movements
+                friend bool operator==(const Iterator& it1, const Iterator& it2) { return it1.ptr == it2.ptr; }
+                friend bool operator!=(const Iterator& it1, const Iterator& it2) { return it1.ptr != it2.ptr; }
+                friend const Iterator operator+(const Iterator& it, int length) // Overload operator + to get the distance of the Iterator position from the pointing position.
+                {
+                    char* ptr1 = it.ptr+length;
+                    return Iterator(&ptr1[0]);  // return the distnace Iterator.
+                }
+        };
+        Iterator begin() { return Iterator(&this->cPtr[0]); }       // Get the beginning of the Iterator.
+        Iterator end()   { return Iterator(&this->cPtr[strlen(this->cPtr)]); }  // Get the end of the Iterator.
+        String Substr(int start, int noofchars) // Get the sub-string of the string.
+        {
+            char str[noofchars+1] = {" "};
+            int index = 0;
+            while(index < noofchars)
+            {
+                str[index] = cPtr[start+index-1];
+                index++;
+            }
+            
+            return String(str);
+        }
 };
 
-int Strings()
+int StringTest()
 {
-    String str1;    // Declare obect - uses default constror.
-    String str2("I am a new String class.");  // Declare object with data - use of Patameterized constror.
-    cout << "str2: " << str2 << endl;       // Uses overloaded operator (<<) to print str2.
-
-    String str3 = str2; // Copy data from str2 into str1 using copy constructor.
-    cout << "str3: " << str2 << endl;
-
-    str1 = str3;        // Assign data from str3 to str1 using assignment operator.
+    String str1;
+    String str2("I am a new and simple String class.");
+    cout << "str2: " << str2 << endl;
+    String str3(str2);
+    cout << "str3: " << str3 << endl;
+    str1 = str3;
+    cout <<"str1: "<< str1 << endl;
+    str1 += " Appending new string.";
     cout << "str1: " << str1 << endl;
-
-    str1 += " Appending more data to String object.";
-    cout << "str1: " << str1 << endl;
-
-    // Display String data using iterator
-    String::iterator it1 = str1.begin();
-    String::iterator it2 = str1.end();
-
-    // Print data using iterator with Prefix ++ increment.
-    for (it1; it1 != it2; ++it1) { cout << *it1 << " "; }
+    
+    cout <<"Display String data with Iterator: " << endl;
+    String::Iterator it1 = str1.begin();
+    String::Iterator it2 = str1.end();
+    
+    for(it1; it1 != it2; ++it1) {  cout << *it1 << " "; }
     cout << endl;
 
     it1 = str1.begin();
-    // Print data using iterator with Postfix ++ increment.
-    for (it1; it1 != it2; it1++) { cout << *it1 << " "; }
+    it2 = str1.begin() + 16;
+    for(it1; it1 != it2; ++it1) {  cout << *it1 << " "; }
     cout << endl;
-
-    // Print data certain length;
-    it1 = str1.begin();
-    it2 = str1.begin() + 10;   // 10 characters to be printed
-    for (it1; it1 != it2; ++it1) { cout << *it1 << " "; }
-    cout << endl;
-
-    // Print data until the position from end.
-    it1 = str1.begin();
-    it2 = str1.end() - 10;   // 10 characters to be printed
-    for (it1; it1 != it2; ++it1) { cout << *it1 << " "; }
-    cout << endl;
-
+    
+    String str4 = str1.Substr(8,5);
+    cout <<"str4: " << str4 << endl;
+    
     return 0;
 }
-
 int main()
 {
-	Strings();
-	return 0;
+    StringTest();
+    return 0;
 }
